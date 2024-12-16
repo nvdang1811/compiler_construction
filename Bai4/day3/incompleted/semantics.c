f95ec1117c8d18bde4a7dd7c12232d7828d8b1cc
nvdang1811 <nvandang1811@gmail.com>
2024-12-16 20:32:46 +0700

    commit semantics

---
+++ Bai4/day3/incompleted/semantics.c
@@ -0,0 +1,107 @@
+/* 
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "semantics.h"
+#include "error.h"
+
+extern SymTab* symtab;
+extern Token* currentToken;
+
+Object* lookupObject(char *name) {
+  // TODO: Implement the logic to look up an object by its name in the current scope and outer scopes
+  Scope* scope = symtab->currentScope;
+  Object* obj;
+
+  while (scope != NULL) {
+    obj = findObject(scope->objList, name);
+    if (obj != NULL) return obj;
+    scope = scope->outer;
+  }
+  obj = findObject(symtab->globalObjectList, name);
+  if (obj != NULL) return obj;
+  return NULL;
+}
+
+void checkFreshIdent(char *name) {
+  // TODO: Implement the logic to check if an identifier is fresh (not already declared in the current scope)
+  if (findObject(symtab->currentScope->objList, name) != NULL)
+    error(ERR_DUPLICATE_IDENT, currentToken->lineNo, currentToken->colNo);  
+}
+
+Object* checkDeclaredIdent(char* name) {
+  // TODO: Implement the logic to check if an identifier has been declared
+  Object* obj = lookupObject(name);
+  if (obj == NULL) {
+    error(ERR_UNDECLARED_IDENT,currentToken->lineNo, currentToken->colNo);
+  }
+  return obj;
+}
+
+Object* checkDeclaredConstant(char* name) {
+  // TODO: Implement the logic to check if a constant has been declared
+  Object* obj = lookupObject(name);
+  if (obj == NULL)
+    error(ERR_UNDECLARED_CONSTANT,currentToken->lineNo, currentToken->colNo);
+  if (obj->kind != OBJ_CONSTANT)
+    error(ERR_INVALID_CONSTANT,currentToken->lineNo, currentToken->colNo);
+
+  return obj;
+}
+
+Object* checkDeclaredType(char* name) {
+  // TODO: Implement the logic to check if a type has been declared
+  Object* obj = lookupObject(name);
+  if (obj == NULL)
+    error(ERR_UNDECLARED_TYPE,currentToken->lineNo, currentToken->colNo);
+  if (obj->kind != OBJ_TYPE)
+    error(ERR_INVALID_TYPE,currentToken->lineNo, currentToken->colNo);
+}
+
+Object* checkDeclaredVariable(char* name) {
+  // TODO: Implement the logic to check if a variable has been declared
+  Object* obj = lookupObject(name);
+  if (obj == NULL)
+    error(ERR_UNDECLARED_VARIABLE,currentToken->lineNo, currentToken->colNo);
+  if (obj->kind != OBJ_VARIABLE)
+    error(ERR_INVALID_VARIABLE,currentToken->lineNo, currentToken->colNo);
+
+  return obj;
+}
+
+Object* checkDeclaredFunction(char* name) {
+  // TODO: Implement the logic to check if a function has been declared
+  Object* obj = lookupObject(name);
+  if (obj == NULL)
+    error(ERR_UNDECLARED_FUNCTION,currentToken->lineNo, currentToken->colNo);
+  if (obj->kind != OBJ_FUNCTION)
+    error(ERR_INVALID_FUNCTION,currentToken->lineNo, currentToken->colNo);
+
+  return obj;
+}
+
+Object* checkDeclaredProcedure(char* name) {
+  // TODO: Implement the logic to check if a procedure has been declared
+  Object* obj = lookupObject(name);
+  if (obj == NULL)
+    error(ERR_UNDECLARED_PROCEDURE,currentToken->lineNo, currentToken->colNo);
+  if (obj->kind != OBJ_PROCEDURE)
+    error(ERR_INVALID_PROCEDURE,currentToken->lineNo, currentToken->colNo);
+
+  return obj;
+}
+
+Object* checkDeclaredLValueIdent(char* name) {
+  // TODO: Implement the logic to check if an identifier is a valid left-value (variable or parameter)
+  Object* obj = lookupObject(name);
+  if (obj == NULL)
+    error(ERR_UNDECLARED_IDENT,currentToken->lineNo, currentToken->colNo);
+  if (obj->kind != OBJ_VARIABLE && obj->kind != OBJ_PARAMETER)
+    error(ERR_INVALID_IDENT,currentToken->lineNo, currentToken->colNo);
+
+  return obj;
+}
